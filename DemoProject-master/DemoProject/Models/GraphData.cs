f257d25805750210246113b521e3b154507a10f8
Divya-T-M <divyatm95@gmail.com>
2024-03-31 15:04:45 +0530

    graph

---
+++ DemoProject-master/DemoProject/Models/GraphData.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace DemoProject.Models
+{
+    public class GraphData
+    {
+        public string Date { get; set; }
+        public double RevVar { get; set; }
+        public double VolVar { get; set; }
+    }
+}
